ada53d2caf327a01ecdaf5f06625f3996d631861
Vishal Tahil <vtahil@AMBITENERGY.COM>
2016-10-26 23:18:35 -0500

    restoring the code which was commmented

--- src/Nano.Demo.AspNet/Global.asax.cs
+++ src/Nano.Demo.AspNet/Global.asax.cs
@@ -34,7 +34,7 @@ namespace Nano.Demo.AspNet
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            //WebApiConfig.Register( GlobalConfiguration.Configuration );
+            WebApiConfig.Register( GlobalConfiguration.Configuration );
             FilterConfig.RegisterGlobalFilters( GlobalFilters.Filters );
             RouteConfig.RegisterRoutes( RouteTable.Routes );
         }
